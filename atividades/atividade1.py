4def154569d120344c8c8aac97ffab425764c68f
Emmanuel Andrade <emmanuel.andrade@gmail.com>
2023-05-21 14:19:03 -0300

    primeira atividade

--- atividades/atividade1.py
+++ atividades/atividade1.py
@@ -7,3 +7,133 @@ env = dotenv_values(".env")
 API_KEY = env["API_KEY_OPENAI"]
 
 print(API_KEY)
+
+openai.api_key = API_KEY
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    
+    """
+    Given a prompt and an optional OpenAI model name, this function generates a
+    chatbot response using the specified model. The chatbot response is based on
+    the prompt and the degree of randomness specified by the temperature
+    parameter. The function returns the chatbot's response as a string.
+    
+    :param prompt: A string representing the prompt for the chatbot.
+    :param model: An optional string representing the name of the OpenAI model to
+                  use. Defaults to "gpt-3.5-turbo".
+                  
+    :return: A string representing the chatbot's response to the given prompt.
+    """
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+# Trabalhando com prompts.
+# Tática 1: Usando delimitadores para indicar claramente as entradas de formas separadas.
+# Os delimitadores podem ser aspas simples, aspas duplas, <>, <tag></tag, :
+
+text = """
+    You should express what you want a model to do by \
+    providing instructions that are as clear and \ 
+    specific as you can possibly make them. \ 
+    This will guide the model towards the desired output, \ 
+    and reduce the chances of receiving irrelevant \ 
+    or incorrect responses. Don't confuse writing a \ 
+    clear prompt with writing a short prompt. \ 
+    In many cases, longer prompts provide more clarity \ 
+    and context for the model, which can lead to \ 
+    more detailed and relevant outputs.
+"""
+prompt = f"""
+Summarize the text delimited by triple backticks \ 
+into a single sentence.
+```{text}```
+"""
+
+response = get_completion(prompt)
+print(response)
+
+
+# Tática 2: Solicitando uma saída estruturada.
+# Neste exemplo é solictada uma saída em JSON.
+
+prompt ="""
+Generate a list of three made-up book titles along \ 
+with their authors and genres. 
+Provide them in JSON format with the following keys: 
+book_id, title, author, genre.
+"""
+response = get_completion(prompt)
+print(response)
+
+
+# Tática 3: Solicitando ao modelo para verificar quando as condições são satisfeitas.
+
+text_1 = f"""
+Making a cup of tea is easy! First, you need to get some \ 
+water boiling. While that's happening, \ 
+grab a cup and put a tea bag in it. Once the water is \ 
+hot enough, just pour it over the tea bag. \ 
+Let it sit for a bit so the tea can steep. After a \ 
+few minutes, take out the tea bag. If you \ 
+like, you can add some sugar or milk to taste. \ 
+And that's it! You've got yourself a delicious \ 
+cup of tea to enjoy.
+"""
+prompt = f"""
+You will be provided with text delimited by triple quotes. 
+If it contains a sequence of instructions, \ 
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \ 
+then simply write \"No steps provided.\"
+
+\"\"\"{text_1}\"\"\"
+"""
+response = get_completion(prompt)
+print("Completion for Text 1:")
+print(response)
+
+# Neste segundo caso a saída será "No steps provided."
+
+text_2 = f"""
+The sun is shining brightly today, and the birds are \
+singing. It's a beautiful day to go for a \ 
+walk in the park. The flowers are blooming, and the \ 
+trees are swaying gently in the breeze. People \ 
+are out and about, enjoying the lovely weather. \ 
+Some are having picnics, while others are playing \ 
+games or simply relaxing on the grass. It's a \ 
+perfect day to spend time outdoors and appreciate the \ 
+beauty of nature.
+"""
+prompt = f"""
+You will be provided with text delimited by triple quotes. 
+If it contains a sequence of instructions, \ 
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \ 
+then simply write \"No steps provided.\"
+
+\"\"\"{text_2}\"\"\"
+"""
+response = get_completion(prompt)
+print("Completion for Text 2:")
+print(response)
+
+
